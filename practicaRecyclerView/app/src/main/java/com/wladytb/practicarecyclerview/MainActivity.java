d58f38b62bfe4d8f20078d0d1ca134c3e73b010e
wladytb <tyrone.tocta2016@uteq.edu.ec>
2021-06-13 22:59:39 -0500

    cardView

--- practicaRecyclerView/app/src/main/java/com/wladytb/practicarecyclerview/MainActivity.java
+++ practicaRecyclerView/app/src/main/java/com/wladytb/practicarecyclerview/MainActivity.java
@@ -3,12 +3,17 @@ package com.wladytb.practicarecyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgview= (ImageView)findViewById(R.id.imgview);
+        imgview.setImageResource(R.drawable.uteq);
     }
 }
